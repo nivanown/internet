f7256f223fb29c50c77ac2f058e38dbf55e57577
Nikita <32540777+nivanown@users.noreply.github.com>
2023-12-17 19:31:25 +0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+﻿var swiper = new Swiper('.text-slider', {
+  spaceBetween: 0,
+  slidesPerView: 4,
+  loop: true,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false,
+  },
+  speed: 4000,
+});
